c15b48def54f23873ba0eba653b769c1109d18cf
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-06-22 19:00:09 +0530

    Add code to select right element to remove

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 // to remove this line
 // <meta name="google" content="notranslate"/>
 
-var element = document.querySelector('div.feedmain');
+var element = document.querySelector('meta[name="google"]');
 element.parentElement.removeChild(element);
